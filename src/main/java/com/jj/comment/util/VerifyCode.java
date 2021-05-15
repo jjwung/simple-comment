b636be95ad739ee4319476d89554e687902fdeff
王军军 <824430884@qq.com>
2021-05-15 23:41:50 +0800

    工具类：生成随机数

---
+++ src/main/java/com/jj/comment/util/VerifyCode.java
@@ -0,0 +1,21 @@
+package com.jj.comment.util;
+
+import java.util.Random;
+
+public class VerifyCode {
+    /**
+     * 生成指定长度的随机纯数字码
+     *
+     * @param len
+     * @return
+     */
+    public static String generateNumberCode(int len) {
+        StringBuilder sb = new StringBuilder();
+        Random random = new Random();
+        for (int i = 0; i < len; i++) {
+            int rn = random.nextInt(10);
+            sb.append(rn);
+        }
+        return sb.toString();
+    }
+}
